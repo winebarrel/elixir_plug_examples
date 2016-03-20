71eb8bbfd8e6c04640a4d3751fd15d3b2e9694ec
winebarrel <sgwr_dts@yahoo.co.jp>
2016-03-20 23:14:07 +0900

    Add Parsers

---
+++ lib/Plug.Parsers/parsers.ex
@@ -0,0 +1,19 @@
+defmodule ElixirPlugExamples.ParsersPlug do
+  use Plug.Builder
+
+  plug Plug.Parsers, parsers: [:urlencoded, :multipart, :json],  json_decoder: Poison
+  plug :resp
+
+  def start do
+    {:ok, _pid} = Plug.Adapters.Cowboy.http __MODULE__, [], port: 9001
+  end
+
+  def resp(conn, _opts) do
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(200, inspect(conn.body_params))
+  end
+end
+
+#mix run --no-halt -e ElixirPlugExamples.ParsersPlug.start
+#curl -H "Content-type: application/json" -X POST -d '{"user":{"first_name":"firstname","last_name":"lastname","email":"email@email.com","password":"app123","password_confirmation":"app123"}}' localhost:9001
--- mix.exs
+++ mix.exs
@@ -28,6 +28,7 @@ defmodule ElixirPlugExamples.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [{:cowboy, "~> 1.0.0"},
-     {:plug, "~> 1.1.2"}]
+     {:plug, "~> 1.1.2"},
+     {:poison, "~> 2.0"}]
   end
 end
